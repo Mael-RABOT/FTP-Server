83723cd500b49203b0dca9679e140610f73150b9
Mael-RABOT <mael.rabot@gmail.com>
2024-03-01 11:11:40 +0100

    fix(file retrival): Fix RETR and STOR file path generation
    Signed-off-by: Mael-RABOT <mael.rabot@gmail.com>

--- src/commands/retr.c
+++ src/commands/retr.c
@@ -12,13 +12,16 @@
 
 #include "../../include/protoype.h"
 
-static void send_file(t_client *client, char **args)
+static void send_file(t_ftp **ftp, t_client *client, char *filename)
 {
     FILE *file;
     char buffer[1024];
     int bytes_read;
+    char *pwd = get_pwd(ftp, &client->socket);
+    char *path = malloc(strlen(pwd) + strlen(filename) + 2);
 
-    file = fopen(args[1], "r");
+    sprintf(path, "%s/%s", pwd, filename);
+    file = fopen(path, "r");
     if (file == NULL)
         return;
     bytes_read = fread(buffer, 1, 1024, file);
@@ -27,14 +30,32 @@ static void send_file(t_client *client, char **args)
         bytes_read = fread(buffer, 1, 1024, file);
     }
     fclose(file);
+    free(path);
 }
 
-static int check_args(char **args)
+static int check_file(char *filename, t_ftp **ftp, int *client_socket)
+{
+    char *pwd = get_pwd(ftp, client_socket);
+    char *path = malloc(strlen(pwd) + strlen(filename) + 2);
+    FILE *file;
+
+    sprintf(path, "%s/%s", pwd, filename);
+    file = fopen(path, "r");
+    if (file == NULL) {
+        free(path);
+        return 1;
+    }
+    fclose(file);
+    free(path);
+    return 0;
+}
+
+static int check_args(char **args, t_ftp **ftp, int *client_socket)
 {
     int size;
 
     for (size = 0; args[size]; size++);
-    return (size != 2) ? 1 : 0;
+    return (size != 2) ? 1 : check_file(args[1], ftp, client_socket);
 }
 
 void retr(t_ftp **ftp, char **arg, int *client_socket)
@@ -42,15 +63,15 @@ void retr(t_ftp **ftp, char **arg, int *client_socket)
     t_client *client = get_client(ftp, client_socket);
     int pid;
 
-    if (check_args(arg))
-        return;
+    if (check_args(arg, ftp, client_socket))
+        return (void) send_to_socket(ftp, C451, client_socket);
     pid = fork();
     if (pid == -1)
         return (void)send_to_socket(ftp, C451, client_socket);
     if (pid == 0) {
         if (launch_data_connections(ftp, client) == 1)
             return;
-        send_file(client, arg);
+        send_file(ftp, client, arg[1]);
         send_to_socket(ftp, C226, client_socket);
         close(client->data_socket);
         exit(EXIT_SUCCESS);
--- src/commands/stor.c
+++ src/commands/stor.c
@@ -12,13 +12,16 @@
 
 #include "../../include/protoype.h"
 
-static void get_file(t_client *client, char *filename)
+static void get_file(t_ftp **ftp, t_client *client, char *filename)
 {
     FILE *file;
     char buffer[1024];
     int bytes_read;
+    char *pwd = get_pwd(ftp, &client->socket);
+    char *path = malloc(strlen(pwd) + strlen(filename) + 2);
 
-    file = fopen(filename, "w");
+    sprintf(path, "%s/%s", pwd, filename);
+    file = fopen(path, "w");
     if (file == NULL)
         return;
     do {
@@ -30,13 +33,29 @@ static void get_file(t_client *client, char *filename)
     fclose(file);
 }
 
-static int check_args(char **args)
+static int check_file(char *filename, t_ftp **ftp, int *client_socket)
+{
+    char *pwd = get_pwd(ftp, client_socket);
+    char *path = malloc(strlen(pwd) + strlen(filename) + 2);
+    FILE *file;
+
+    sprintf(path, "%s/%s", pwd, filename);
+    file = fopen(path, "r");
+    if (file == NULL) {
+        free(path);
+        return 1;
+    }
+    fclose(file);
+    free(path);
+    return 0;
+}
+
+static int check_args(char **args, t_ftp **ftp, int *client_socket)
 {
     int size;
 
-    for (size = 0; args[size]; size++)
-        ;
-    return (size != 2) ? 1 : 0;
+    for (size = 0; args[size]; size++);
+    return (size != 2) ? 1 : check_file(args[1], ftp, client_socket);
 }
 
 void stor(t_ftp **ftp, char **arg, int *client_socket)
@@ -44,15 +63,15 @@ void stor(t_ftp **ftp, char **arg, int *client_socket)
     t_client *client = get_client(ftp, client_socket);
     int pid;
 
-    if (check_args(arg))
-        return;
+    if (check_args(arg, ftp, client_socket))
+        return (void) send_to_socket(ftp, C451, client_socket);
     pid = fork();
     if (pid == -1)
         return (void)send_to_socket(ftp, C451, client_socket);
     if (pid == 0) {
         if (launch_data_connections(ftp, client) == 1)
             return;
-        get_file(client, arg[1]);
+        get_file(ftp, client, arg[1]);
         send_to_socket(ftp, C226, client_socket);
         close(client->data_socket);
         exit(EXIT_SUCCESS);
